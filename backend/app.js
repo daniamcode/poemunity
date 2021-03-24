ab1e52f4c1573cf0ccd188e94121fff0b0b1ec66
Dani Alcala <daniamcode@gmail.com>
2021-03-24 13:13:07 +0100

    update express server in order to consider proper routing on production

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 const Poem = require('./src/models/poemModel');
 
+if (process.env.NODE_ENV === 'production') {
 app.use(express.static('public'));
+}
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
@@ -21,6 +23,12 @@ app.use('/api/poems', poemRouter)
 app.listen(port, () => debug(`running on port ${port}`));
 
 const path = require('path');
+if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
+} else {
+	app.get('/', (req, res) => {
+		res.send('Server is ok')
+	});
+}
