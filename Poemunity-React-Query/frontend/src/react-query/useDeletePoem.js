d6600702363755cdcf454394aa3ab165b5566a52
Dani Alcala <daniamcode@gmail.com>
2021-10-31 13:54:23 +0100

    use context.config

--- Poemunity-React-Query/frontend/src/react-query/useDeletePoem.js
+++ Poemunity-React-Query/frontend/src/react-query/useDeletePoem.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
 import { useMutation, useQueryClient } from 'react-query';
+import { useContext } from 'react';
+import { AppContext } from '../App';
 
 export default function useDeletePoem() {
  const queryClient = useQueryClient()
+ const context = useContext(AppContext);
   return useMutation(
-    (id) => axios.delete(`/api/poems/${id}`).then((res) => res.data),
+    (id) => axios.delete(`/api/poems/${id}`, context.config).then((res) => res.data),
     {
       onSuccess: (data, poemDeleted) => {
         // const oldPoems = queryClient.getQueryData('poems')
