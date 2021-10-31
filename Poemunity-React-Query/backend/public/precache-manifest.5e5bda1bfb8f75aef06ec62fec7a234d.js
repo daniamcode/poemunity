4c3c67cd6f12c122f908f4782d1da7cca4e72b92
Dani Alcala <daniamcode@gmail.com>
2021-10-31 21:12:46 +0100

    allow build files

---
+++ Poemunity-React-Query/backend/public/precache-manifest.5e5bda1bfb8f75aef06ec62fec7a234d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec57c14dc0c139a44b860f385cde792d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed9c133d0819ddabb177",
+    "url": "/static/css/main.d11c61dc.chunk.css"
+  },
+  {
+    "revision": "76b346e32b93f7ed7155",
+    "url": "/static/js/2.cf46d065.chunk.js"
+  },
+  {
+    "revision": "edd9a6d0fd78f0d308519aaec3144040",
+    "url": "/static/js/2.cf46d065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed9c133d0819ddabb177",
+    "url": "/static/js/main.7f106e51.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "b6fe1d498d80271a34600b9542e92bdb",
+    "url": "/static/media/login.b6fe1d49.svg"
+  },
+  {
+    "revision": "008cfb2c593b601b9577817547d17ffd",
+    "url": "/static/media/logout.008cfb2c.svg"
+  },
+  {
+    "revision": "6764602838750807ca6ad9d1f83ea4b0",
+    "url": "/static/media/poemunity.67646028.svg"
+  },
+  {
+    "revision": "d17c2f24c0a44f7ab4104ffa32df3e24",
+    "url": "/static/media/profile.d17c2f24.svg"
+  },
+  {
+    "revision": "a8cd76c1934dc020e510f93a33774305",
+    "url": "/static/media/red-heart.a8cd76c1.svg"
+  },
+  {
+    "revision": "70ebbcd163470cdcdd46617ad9e91610",
+    "url": "/static/media/white-heart.70ebbcd1.svg"
+  }
+]);
