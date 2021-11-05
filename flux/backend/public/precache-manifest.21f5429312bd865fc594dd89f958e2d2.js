709af6f81b3ae61f6a69dc622b32f0b493eb0e0a
Dani Alcala <daniamcode@gmail.com>
2021-11-05 22:25:01 +0100

    Revert "test github actions again"
    This reverts commit ee603a0f309c6278713cf125aa1b849a075bcbf8.

---
+++ flux/backend/public/precache-manifest.21f5429312bd865fc594dd89f958e2d2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce4d9b5272e939276d4ba0c552e02bc",
+    "url": "https://poemunity.com/index.html"
+  },
+  {
+    "revision": "853fe487c7840ce37efd",
+    "url": "https://poemunity.com/static/css/main.18af3b7c.chunk.css"
+  },
+  {
+    "revision": "8b70ec4e41202e117180",
+    "url": "https://poemunity.com/static/js/2.52b149af.chunk.js"
+  },
+  {
+    "revision": "6d5732b2feac8e5e7de4bdef582a9cab",
+    "url": "https://poemunity.com/static/js/2.52b149af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "853fe487c7840ce37efd",
+    "url": "https://poemunity.com/static/js/main.fee0cb38.chunk.js"
+  },
+  {
+    "revision": "7f7f69a14acde8060b3c",
+    "url": "https://poemunity.com/static/js/runtime-main.0bf47b77.js"
+  },
+  {
+    "revision": "b6fe1d498d80271a34600b9542e92bdb",
+    "url": "https://poemunity.com/static/media/login.b6fe1d49.svg"
+  },
+  {
+    "revision": "008cfb2c593b601b9577817547d17ffd",
+    "url": "https://poemunity.com/static/media/logout.008cfb2c.svg"
+  },
+  {
+    "revision": "6764602838750807ca6ad9d1f83ea4b0",
+    "url": "https://poemunity.com/static/media/poemunity.67646028.svg"
+  },
+  {
+    "revision": "d17c2f24c0a44f7ab4104ffa32df3e24",
+    "url": "https://poemunity.com/static/media/profile.d17c2f24.svg"
+  },
+  {
+    "revision": "a8cd76c1934dc020e510f93a33774305",
+    "url": "https://poemunity.com/static/media/red-heart.a8cd76c1.svg"
+  },
+  {
+    "revision": "70ebbcd163470cdcdd46617ad9e91610",
+    "url": "https://poemunity.com/static/media/white-heart.70ebbcd1.svg"
+  }
+]);
