91f5f3848442a92612fa525d148ee8490de23a21
Dani Alcala <daniamcode@gmail.com>
2021-11-08 00:35:58 +0100

    test ci on backend

--- Poemunity-React-Query/backend/app.js
+++ Poemunity-React-Query/backend/app.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 console.log(process.env.NODE_ENV)
 console.log(process.env)
-// console.log('test1')
+// console.log('test3')
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('public'))
