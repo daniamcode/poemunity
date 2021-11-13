ffb168ca7e745ce24e13f2589efc842d5de8239d
Dani Alcala <daniamcode@gmail.com>
2021-11-14 00:33:48 +0100

    minor test

--- Poemunity-React-Query/frontend/src/data/constants.js
+++ Poemunity-React-Query/frontend/src/data/constants.js
@@ -1,4 +1,4 @@
-export const WEB_SUBTITLE = 'Your poem community-m'
+export const WEB_SUBTITLE = 'Your poem community-mas'
 export const CATEGORIES_TITLE = 'Categories'
 export const CATEGORIES = ['Anniversary', 'Friendship', 'Funny', 'Haikus', 'Hope', 'Life', 'Love', 'Sad']
 export const ALL = 'All'
