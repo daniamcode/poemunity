87b99e90c5cd1c1694221c931eaa04102e9aaec8
Dani Alcala <daniamcode@gmail.com>
2021-11-19 20:18:36 +0100

    add jwt to useSavePoem

--- Poemunity-React-Query/frontend/src/react-query/useSavePoem.js
+++ Poemunity-React-Query/frontend/src/react-query/useSavePoem.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 import { useMutation, useQueryClient } from 'react-query';
+import { useContext } from 'react';
+import { AppContext } from '../App';
 
 export default function useSavePoem() {
  const queryClient = useQueryClient()
+ const context = useContext(AppContext);
+
   return useMutation(
     // arguments have to be a single variable or object in this case
-    ({poem, poemId}) => axios.patch(`/api/poems/${poemId}`, poem).then((res) => res.data),
+    ({poem, poemId}) => axios.patch(`/api/poems/${poemId}`, poem, context.config).then((res) => res.data),
     {
       onSuccess: () => {        
         queryClient.invalidateQueries('poems')
