afbb9be451854eaf5053e827ed348f52943fe2a3
Dani Alcala <daniamcode@gmail.com>
2022-04-02 01:01:53 +0200

    refactor login post

--- Poemunity-React-Query/backend/src/controllers/login.js
+++ Poemunity-React-Query/backend/src/controllers/login.js
@@ -7,32 +7,31 @@ loginRouter.post('/', async (request, response) => {
   const { body } = request
   const { username, password } = body
 
-  const user = await User.findOne({ username })
+  let user = await User.findOne({ username })
 
-  const passwordCorrect = user === null
+  let passwordCorrect = user === null
     ? false
     : await bcrypt.compare(password, user.passwordHash)
-
+    
   if (!(user && passwordCorrect)) {
     response.status(401).json({
-      error: 'invalid user or password'
-    })
-  }
-
-  const userForToken = {
-    id: user._id,
-    username: user.username
-  }
-
-  const token = jwt.sign(
-    userForToken,
-    process.env.SECRET,
-    {
-      expiresIn: 60 * 60 * 24 * 7
+    error: 'invalid user or password'})
+  } else {
+    const userForToken = {
+      id: user._id,
+      username: user.username
     }
-  )
-
-  response.send(token)
+  
+    const token = jwt.sign(
+      userForToken,
+      process.env.SECRET,
+      {
+        expiresIn: 60 * 60 * 24 * 7
+      }
+    )
+  
+    response.send(token)
+  }
 })
 
 module.exports = loginRouter
