29daa0393d13f7282760d67a1e297df47e5afbc3
Dani Alcala <daniamcode@gmail.com>
2022-06-30 21:37:11 +0200

    change name of ts files

---
+++ poemunity-redux/frontend/src/typescript/interfaces.d.ts
@@ -0,0 +1,25 @@
+export interface Poem {
+    id: string
+    author: string
+    date: string
+    genre: string
+    likes: string[]
+    picture: string
+    poem: string
+    title: string
+    userId: string
+  }
+
+export interface PoemQuery {
+    isFetching: boolean
+    isError: boolean
+    item?: Poem
+  }
+
+export interface RootState {
+    poemQuery: PoemQuery
+  }
+
+export interface Props {
+    match: {params: {poemId: string}}
+  }
