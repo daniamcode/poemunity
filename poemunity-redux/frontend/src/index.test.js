4de6ba4e3a79d15e869693e621ebf034b18f0734
Dani Alcala <daniamcode@gmail.com>
2022-07-04 20:47:23 +0200

    change "it" for "test"

--- poemunity-redux/frontend/src/index.test.js
+++ poemunity-redux/frontend/src/index.test.js
@@ -4,7 +4,7 @@ import './index'
 jest.mock("react-dom", () => ({ render: jest.fn() }));
 
 describe("Application root", () => {
-  it("should render", () => {
+  test("should render", () => {
     const root = document.getElementById('root');
     if(root) {
       expect(ReactDOM.render).toHaveBeenCalled();
