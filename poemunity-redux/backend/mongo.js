b4ef9bdfc139cb8aa1a3ed5ab48498e78b9f4992
Dani Alcala <daniamcode@gmail.com>
2022-07-08 23:02:33 +0200

    update mongoose version

--- poemunity-redux/backend/mongo.js
+++ poemunity-redux/backend/mongo.js
@@ -3,22 +3,20 @@ const mongoose = require('mongoose')
 const { MONGODB, MONGODB_PRE, NODE_ENV } = process.env
 
 const connectionString = NODE_ENV === 'development'
-? MONGODB_PRE
-: MONGODB
+  ? MONGODB_PRE
+  : MONGODB
 
 mongoose.connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Database connected')
+  }).catch(err => {
+    console.error(err)
   })
-    .then(() => {
-      console.log('Database connected')
-    }).catch(err => {
-      console.error(err)
-    })
-  
-  process.on('uncaughtException', error => {
-    console.error(error)
-    mongoose.disconnect()
-  })
+
+process.on('uncaughtException', error => {
+  console.error(error)
+  mongoose.disconnect()
+})
--- poemunity-redux/backend/src/controllers/poems.js
+++ poemunity-redux/backend/src/controllers/poems.js
@@ -5,14 +5,14 @@ const User = require('../models/User')
 const userExtractor = require('../middleware/userExtractor')
 
 poemsRouter.get('/', async (req, res) => {
-  if(req.query.origin) {
-    Poem.find({origin: req.query.origin}, (error, poems) => {
+  if (req.query.origin) {
+    Poem.find({ origin: req.query.origin }, (error, poems) => {
       if (error) {
         res.send(error)
       } else {
         res.json(poems)
       }
-    })  
+    })
   } else {
     Poem.find((error, poems) => {
       if (error) {
@@ -29,21 +29,21 @@ poemsRouter.post('/', userExtractor, async (req, res) => {
   const poem = req.body
   const { userId } = req
 
-  const adminId = process.env.NODE_ENV === 'development' 
-  ? process.env.REACT_APP_ADMIN_PRE
-  : process.env.REACT_APP_ADMIN
+  const adminId = process.env.NODE_ENV === 'development'
+    ? process.env.REACT_APP_ADMIN_PRE
+    : process.env.REACT_APP_ADMIN
 
   const fakeUsers = [
-    {id: 1, username: 'cathy2', name: 'Catherine Cawley', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/catherine-cawley.jpg'},
-    {id: 2, username: 'StaceyCosgrove', name: 'Stacey Cosgrove', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/stacey-cosgrove.jpg'},
-    {id: 3, username: 'sarahGR', name: 'Sarah Griffin', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/sarah-griffin.jpg'},
-    {id: 4, username: 'rayson', name: 'Ray Higgins', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/ray-higgins.jpg'},
-    {id: 5, username: 'typp', name: 'Ty Lue', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/ty-lue.jpg'},
-    {id: 6, username: 'John Donne', name: 'John Donne', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/john-donne.jpg'},
-    {id: 7, username: 'Walt Whitman', name: 'Walt Whitman', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/walt-whitman.jpg'},
+    { id: 1, username: 'cathy2', name: 'Catherine Cawley', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/catherine-cawley.jpg' },
+    { id: 2, username: 'StaceyCosgrove', name: 'Stacey Cosgrove', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/stacey-cosgrove.jpg' },
+    { id: 3, username: 'sarahGR', name: 'Sarah Griffin', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/sarah-griffin.jpg' },
+    { id: 4, username: 'rayson', name: 'Ray Higgins', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/ray-higgins.jpg' },
+    { id: 5, username: 'typp', name: 'Ty Lue', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/ty-lue.jpg' },
+    { id: 6, username: 'John Donne', name: 'John Donne', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/john-donne.jpg' },
+    { id: 7, username: 'Walt Whitman', name: 'Walt Whitman', picture: 'https://poemunity.s3.us-east-2.amazonaws.com/user/walt-whitman.jpg' },
   ]
 
-  const fakeUser = (poem.userId && userId === adminId) ? fakeUsers.find(user=>user.id === parseInt(poem.userId)) : null
+  const fakeUser = (poem.userId && userId === adminId) ? fakeUsers.find(user => user.id === parseInt(poem.userId)) : null
 
   const user = await User.findById(userId)
 
@@ -61,19 +61,23 @@ poemsRouter.post('/', userExtractor, async (req, res) => {
 
     res.status(201)
     res.json(savedPoem)
-  } catch {}
+  } catch {
+    console.log('backend error')
+    console.log(error)
+  }
 })
 
 // add new property to all existing poems
 poemsRouter.patch('/', async (req, res) => {
   const newProperty = req.body
 
-  const response = await Poem.updateMany({$set: newProperty});
+  const response = await Poem.updateMany({ $set: newProperty });
 
   if (response === null) {
     return res.status(404).json({
-    error: 'error'
-  })}
+      error: 'error'
+    })
+  }
   console.log(newProperty)
   res.status(204).end()
 })
