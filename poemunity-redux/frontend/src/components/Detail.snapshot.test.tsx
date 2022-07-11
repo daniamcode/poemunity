a1d1c856bb35e6e9195797f43683b2ad73a2c706
Dani Alcala <daniamcode@gmail.com>
2022-07-12 00:59:52 +0200

    migrate snapshot test to tsx

--- poemunity-redux/frontend/src/components/Detail.snapshot.test.tsx
+++ poemunity-redux/frontend/src/components/Detail.snapshot.test.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
+import { render } from '@testing-library/react';
 import Detail from './Detail'
 import { BrowserRouter } from 'react-router-dom'
 import {
@@ -10,17 +9,19 @@ import { Provider } from 'react-redux';
 import store from '../redux/store';
 
 
-function renderDetail (arg) {
+function renderDetail(arg?: object) {
   const defaultProps = {
     match: {
-      params: {}
+      params: {
+        poemId: '1',
+      }
     }
   }
 
   const props = { ...defaultProps, ...arg }
   const queryClient = new QueryClient();
 
-  return renderer.create(
+  return render(
     <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
@@ -32,7 +33,7 @@ function renderDetail (arg) {
 }
 
 describe('Detail', () => {
-  let DetailTree
+  let DetailTree = {}
 
   beforeEach(async () => {
     DetailTree = renderDetail()
