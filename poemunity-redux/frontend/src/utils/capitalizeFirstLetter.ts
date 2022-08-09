ffe6ad774253ac2e9e036d0afdb7eb12dafb4f09
Dani Alcala <daniamcode@gmail.com>
2022-08-09 17:59:04 +0200

    add jsdoc

--- poemunity-redux/frontend/src/utils/capitalizeFirstLetter.ts
+++ poemunity-redux/frontend/src/utils/capitalizeFirstLetter.ts
@@ -1,3 +1,7 @@
+/** 
+ * @param {string} string - string to capitalize first letter
+ * @returns {string} string with first letter capitalized
+ */
 function capitalizeFirstLetter (string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
