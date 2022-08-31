2d831f121ffcd79ac1e6a92606fd5adc50605ca4
Dani Alcala <daniamcode@gmail.com>
2022-08-31 17:16:55 +0200

    toggle expanded accordion

--- poemunity-redux/frontend/src/components/SimpleAccordion.tsx
+++ poemunity-redux/frontend/src/components/SimpleAccordion.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import Accordion from '@material-ui/core/Accordion'
 import AccordionSummary from '@material-ui/core/AccordionSummary'
 import AccordionDetails from '@material-ui/core/AccordionDetails'
@@ -7,23 +8,41 @@ import { Link } from 'react-router-dom'
 import { CATEGORIES_TITLE, CATEGORIES, ALL } from '../data/constants'
 
 export default function SimpleAccordion() {
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded)
+  }
   return (
-    <Accordion className='accordion'>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls='panel1a-content' id='panel1a-header'>
+    <Accordion className='accordion' expanded={expanded}>
+      <AccordionSummary 
+        expandIcon={<ExpandMoreIcon />} 
+        aria-controls='panel1a-content' 
+        id='panel1a-header'
+        onClick={toggleExpanded}
+      >
         <div className='header__dropdown-categories-icon'></div>
         <p className='header__dropdown-categories'>{CATEGORIES_TITLE}</p>
       </AccordionSummary>
       <div>
         {CATEGORIES?.sort().map((category) => (
           <AccordionDetails key={category}>
-            <Link className='header__dropdown-subcategories' to={`/${category.toLowerCase()}`}>
+            <Link 
+              className='header__dropdown-subcategories' 
+              to={`/${category.toLowerCase()}`}
+              onClick={toggleExpanded}
+            >
               {category}
             </Link>
           </AccordionDetails>
         ))}
       </div>
       <AccordionDetails>
-        <Link className='header__dropdown-subcategories' to='/'>
+        <Link 
+          className='header__dropdown-subcategories' 
+          to='/'
+          onClick={toggleExpanded}
+        >
           {ALL}
         </Link>
       </AccordionDetails>
