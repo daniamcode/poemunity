429c87dde656c2035d8892c28d69cd38808dc654
alcalamu <dani.alcalamund@webfleet.com>
2024-07-02 19:39:53 +0200

    remove unused code

--- frontend/build.js
+++ frontend/build.js
@@ -2,13 +2,6 @@
 const { build, context } = require('esbuild')
 const { sassPlugin } = require('esbuild-sass-plugin')
 
-const {
-    esbuildPluginLiveReload,
-} = require('./utils/webserver.js')
-
-const { createEsbuildDefaultConfig } = require('./conf/esbuild.config.ts')
-
-const RELOAD_PORT = 8080
 const argPattern = /--(\w+)(?:=(\w+))?/
 
 function parseArguments() {
@@ -23,29 +16,7 @@ function parseArguments() {
 
 const config = parseArguments()
 const isWatchMode = config['watch'] !== undefined
-const isReloadMode = config['reload'] !== undefined
 const isDev = process.env.NODE_ENV !== 'production'
-const cspWsLocalhost = isDev ? `http://localhost:${RELOAD_PORT}` : ''
-
-const specificConfig = {
-    entryPoints: ['src/index.tsx'],
-    minify: !isDev,
-    outdir: './build',
-    sourcemap: isDev,
-    splitting: true
-}
-
-// const esbuildConfigBase = createEsbuildDefaultConfig(null, specificConfig)
-
-// const esbuildConfigReload = {
-//     ...esbuildConfigBase,
-//     banner: {
-//         js: `
-//             (() => new EventSource('http://localhost:${RELOAD_PORT}/live').addEventListener('message', e => location.reload()))();
-//         `
-//     },
-//     plugins: [...esbuildConfigBase.plugins, esbuildPluginLiveReload({ port: RELOAD_PORT })]
-// }
 
 const esbuildConfig = {
   define: {
@@ -68,12 +39,9 @@ const esbuildConfig = {
     '.woff2': 'dataurl'
   },
   logLevel: 'info',
-//   plugins: [sassPlugin(), esbuildPluginLiveReload({ port: RELOAD_PORT })],
   plugins: [sassPlugin()],
   target: ['es2020'],
 }
-
-// const esbuildConfig = isReloadMode ? esbuildConfigReload : esbuildConfigBase
 
 if (isWatchMode) {
     context(esbuildConfig).then(ctx => {
